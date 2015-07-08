0885aaee1888118e2ae6cd0d31bba8c422ae335f
jcnast <j.nast@hotmail.com>
2015-07-08 00:06:05 -0400

    slight change

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -17,8 +17,9 @@ public class MapManager : MonoBehaviour {
 		mapTiles = mapCreator.getMapElements;
 		for(int i = 0; i < numRows; i++){
 			for(int j = 0; j < numCols; j++){
-				Debug.Log(mapTiles[i,j].GetComponent<BaseTile>().tileType);
-				Debug.Log(mapTiles[i,j].GetComponent<BaseTile>().MapPosn.x);
+				BaseTile curTile = mapTiles[i,j].GetComponent<BaseTile>();
+				Debug.Log(curTile.tileType);
+				Debug.Log(curTile.MapPosn);
 			}
 		}
 	}
